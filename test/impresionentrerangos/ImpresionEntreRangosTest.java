e4f7865dfb48b77db60abc46588353eb7e881fca
kevdag21 <kevin.dominguez2002@hotmail.com>
2023-06-11 16:53:03 -0600

    Revert "Console only pair numbers by Kevdag21"
    This reverts commit a641241565f89a7ccb9341f62d2c49d0e7b1f915.

--- test/impresionentrerangos/ImpresionEntreRangosTest.java
+++ test/impresionentrerangos/ImpresionEntreRangosTest.java
@@ -14,11 +14,11 @@ public class ImpresionEntreRangosTest {
      */
     @org.junit.Test
     public void testMain() {
-        int[] resultado = ImpresionEntreRangos.imprimirNumerosPares(3, 8);
-        int[] esperado = {4, 6};
+        int[] resultado = ImpresionEntreRangos.imprimirNumeros(2, 6);
+        int[] esperado = {3, 4, 5};
         Assertions.assertArrayEquals(esperado, resultado);
         
-        int[] resultado2 = ImpresionEntreRangos.imprimirNumerosPares(9, 11);
+        int[] resultado2 = ImpresionEntreRangos.imprimirNumeros(2, 2);
         int[] esperado2 = {};
         Assertions.assertArrayEquals(esperado2, resultado2);
     }
